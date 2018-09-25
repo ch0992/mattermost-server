86806e1a602288f223d6210466682bd94d68d50c
Martin Kraft <martinkraft@gmail.com>
2018-09-25 15:11:30 -0400

    WIP

---
+++ api4/group_test.go
@@ -0,0 +1,27 @@
+// Copyright (c) 2018-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package api4
+
+import "testing"
+
+func TestCreateGroup(t *testing.T) {}
+func TestGetGroup(t *testing.T)    {}
+func TestGetGroups(t *testing.T)   {}
+func TestUpdateGroup(t *testing.T) {}
+func TestDeleteGroup(t *testing.T) {}
+
+func TestCreateGroupMember(t *testing.T) {}
+func TestDeleteGroupMember(t *testing.T) {}
+
+func TestCreateGroupTeam(t *testing.T) {}
+func TestGetGroupTeam(t *testing.T)    {}
+func TestGetGroupTeams(t *testing.T)   {}
+func TestUpdateGroupTeam(t *testing.T) {}
+func TestDeleteGroupTeam(t *testing.T) {}
+
+func TestCreateGroupChannel(t *testing.T) {}
+func TestGetGroupChannel(t *testing.T)    {}
+func TestGetGroupChannels(t *testing.T)   {}
+func TestUpdateGroupChannel(t *testing.T) {}
+func TestDeleteGroupChannel(t *testing.T) {}
